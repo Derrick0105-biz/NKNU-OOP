78b2b8d1b582449e2962bd7ab73b0bf58b843a04
Derrick <derrick0105.cg@gmail.com>
2019-10-23 21:48:43 +0800

    測試1

--- src/A410577036_5_1.java
+++ src/A410577036_5_1.java
@@ -1,5 +1,8 @@
 public class A410577036_5_1 {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        for(int i=0;i<20;i++){
+            System.out.println(i);
+        }
     }
 }
